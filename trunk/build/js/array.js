286483718583854722854733c335ddd4a5ebc8f8
xushengs@gmail.com <xushengs@gmail.com@8db31cd6-5a37-11de-bc7c-c9df4fa8b3c6>
2009-09-01 10:31:12 +0000

    continue working...

---
+++ trunk/build/js/array.js
@@ -0,0 +1,17 @@
+﻿(function($) {
+    // add to loaded module-list
+    $.register('array', '1.0.0.0');
+
+    Array.implement({
+        each: function(fn, bind) {
+            for (var i = 0, l = this.length; i < l; i++) fn.call(bind, i, this[i], this);
+        },
+
+        contains: function(item, from) {
+            return this.indexOf(item, from) != -1;
+        }
+    });
+
+    Array.alias('forEach', 'each');
+
+})(JUI);
