9f01ff05659096e8a8365a641137ef9484c0c279
xushengs@gmail.com <xushengs@gmail.com@8db31cd6-5a37-11de-bc7c-c9df4fa8b3c6>
2010-07-23 12:09:36 +0000

    move source code from build to src

---
+++ src/event.js
@@ -0,0 +1,139 @@
+﻿/*
+* Author:
+*   xushengs@gmail.com
+*   http://fdream.net/
+* */
+(function($) {
+
+    var keys = {
+        '8': 'backspace',
+        '9': 'tab',
+        '13': 'enter',
+        '27': 'esc',
+        '32': 'space',
+        '38': 'up',
+        '40': 'down',
+        '37': 'left',
+        '39': 'right',
+        '46': 'delete'
+    };
+    
+    ///<class>
+    ///    <name>$.Event</name>
+    ///    <summary>
+    ///         提供封装好Event对象。
+    ///    </summary>
+    ///    <include>$</include>
+    ///</class>
+    var Event = function(event) {
+
+        if (event.$family === 'event') {
+            return event;
+        }
+
+        var doc = document, win = window, type = event.type;
+        var target = event.target || event.srcElement;
+        while (target && target.nodeType == 3) {
+            target = target.parentNode;
+        }
+
+        if (/key/.test(type)) {
+            var code = event.which || event.keyCode;
+            var key = keys[code];
+            if (type == 'keydown') {
+                var fKey = code - 111;
+                if (fKey > 0 && fKey < 13) key = 'f' + fKey;
+            }
+            key = key || String.fromCharCode(code).toLowerCase();
+        }
+        else if (type.match(/(click|mouse|menu)/i)) {
+            doc = (!doc.compatMode || doc.compatMode == 'CSS1Compat') ? doc.documentElement : doc.body;
+            var page = {
+                x: event.pageX || event.clientX + doc.scrollLeft,
+                y: event.pageY || event.clientY + doc.scrollTop
+            };
+            var client = {
+                x: (event.pageX) ? event.pageX - win.pageXOffset : event.clientX,
+                y: (event.pageY) ? event.pageY - win.pageYOffset : event.clientY
+            };
+            if (type.match(/DOMMouseScroll|mousewheel/)) {
+                var wheel = (event.wheelDelta) ? event.wheelDelta / 120 : -(event.detail || 0) / 3;
+            }
+            var rightClick = (event.which == 3) || (event.button == 2);
+        }
+
+        return (function(a, b) {
+            for (var p in b) {
+                a[p] = b[p];
+            }
+            return a;
+        })(this, {
+            event: event,
+            type: type,
+
+            page: page,
+            client: client,
+            rightClick: rightClick,
+
+            wheel: wheel,
+
+            target: target,
+
+            code: code,
+            key: key,
+
+            shift: event.shiftKey,
+            control: event.ctrlKey,
+            alt: event.altKey,
+            meta: event.metaKey
+        });
+    };
+
+    $.Native.initialize({
+        name: 'Event',
+        initialize: Event,
+        protect: true
+    });
+
+    Event.implement({
+        stop: function() {
+            ///<summary>
+            /// 终止所有类型事件传播，返回当前对象。
+            ///</summary>
+            ///<returns type="$.Event" />
+            return this.stopPropagation().preventDefault();
+        },
+
+        stopPropagation: function() {
+            ///<summary>
+            /// 终止事件冒泡，返回当前对象。
+            ///</summary>
+            ///<returns type="$.Event" />
+            if (this.event.stopPropagation) {
+                this.event.stopPropagation();
+            }
+            else {
+                this.event.cancelBubble = true;
+            }
+
+            return this;
+        },
+
+        preventDefault: function() {
+            ///<summary>
+            /// 终止事件传播，返回当前对象。
+            ///</summary>
+            ///<returns type="$.Event" />
+            if (this.event.preventDefault) {
+                this.event.preventDefault();
+            }
+            else {
+                this.event.returnValue = false;
+            }
+
+            return this;
+        }
+    });
+
+    $.Event = Event;
+})(JUI);
---
+++ src/window.js
@@ -0,0 +1,87 @@
+﻿/*
+* Author:
+*   xushengs@gmail.com
+*   http://fdream.net/
+* */
+(function($) {
+    // add to loaded module-list
+    //$.register('window', '1.0.0.0');
+
+    ///<class>
+    ///    <name>$.Window</name>
+    ///    <summary>
+    ///         提供封装好window对象，并提供常用的DOM操作方法。
+    ///    </summary>
+    ///    <include>$</include>
+    ///</class>
+
+    var Window = function(win) {
+        if (win && !win.$family) {
+            var proto = Window.prototype;
+            for (var p in proto) {
+                win[p] = proto[p];
+            }
+        }
+        return win;
+    };
+
+    $.Native.initialize({
+        name: 'Window',
+        legacy: window.Window ? window.Window : null,
+        initialize: Window,
+        afterImplement: function(property, value) {
+            window[property] = Window.prototype[property] = value;
+        }
+    });
+
+    /**
+    * window.dimension.js
+    * */
+    Window.implement({
+        dimension: function(sz) {
+            ///<summary>
+            /// 设置或者获取窗口大小
+            ///</summary>
+            ///<param name="style" type="Object">
+            ///	带width和height属性的对象[可选]
+            /// 若不提供，则获取大小。
+            ///</param>
+            ///<returns type="object">带width和height属性的对象</returns>
+            var ww = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth,
+                wh = document.documentElement.clientHeight || document.body.clientHeight || window.innerHeight;
+
+            if (sz) {
+                ww = sz.width === undefined ? ww : sz.width;
+                wh = sz.height === undefined ? wh : sz.height;
+                window.resizeTo(ww, wh);
+                return this;
+            }
+
+            return { width: ww, height: wh };
+
+        },
+
+        scrollPos: function(pos) {
+            ///<summary>
+            /// 设置或者获取窗口滚动条位置
+            ///</summary>
+            ///<param name="style" type="Object">
+            ///	带x和y属性的对象[可选]
+            /// 若不提供，则获取大小。
+            ///</param>
+            ///<returns type="object">带x和y属性的对象</returns>
+            if (pos) {
+                (pos.x !== undefined) && (window.scrollLeft = pos.x);
+                (pos.y !== undefined) && (window.scrollTop = pos.y);
+                return this;
+            }
+
+            var sx = document.documentElement.scrollLeft || document.body.scrollLeft,
+                sy = document.documentElement.scrollTop || document.body.scrollTop;
+            return { x: sx, y: sy };
+        }
+    });
+
+    //new Window(window);
+    $.Window = Window;
+})(JUI);
